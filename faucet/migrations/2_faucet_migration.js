d25f4f09a23f04826ef41ec38c73041fa46935bf
gs337 <remi.brauge@partnre.com>
2022-06-08 14:11:22 +0200

    init truffle

---
+++ faucet/migrations/2_faucet_migration.js
@@ -0,0 +1,7 @@
+/* const { artifacts } = require("truffle"); */
+
+const FaucetContract = artifacts.require("Faucet")
+
+module.exports = function (deployer) {
+    deployer.deploy(FaucetContract)
+}
