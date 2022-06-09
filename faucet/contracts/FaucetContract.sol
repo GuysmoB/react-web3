0f56cd26059cf8bd629f08823b13c2a6e79718f1
gs337 <remi.brauge@partnre.com>
2022-06-09 16:58:15 +0200

    getAllFudners

--- faucet/contracts/FaucetContract.sol
+++ faucet/contracts/FaucetContract.sol
@@ -2,8 +2,15 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
-    // storage variables
-    uint256 public funds = 1000; // only positive values
-    int256 public counter = -10;
-    uint32 public test = 4294967295;
+    address[] public funders;
+
+    receive() external payable {}
+
+    function addFunds() external payable {
+        funders.push(msg.sender);
+    }
+
+    function getAllFunders() external view returns (address[] memory) {
+        return funders;
+    }
 }
