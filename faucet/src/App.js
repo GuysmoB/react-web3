b2c3075369b01cdc12e4bec7b14b807d1559c42d
gs337 <remi.brauge@partnre.com>
2022-06-14 17:25:14 +0200

    css

--- faucet/src/App.js
+++ faucet/src/App.js
@@ -1,9 +1,31 @@
+import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const loadProvider = async () => {};
+  }, []);
+
   return (
-    <div className="App">
-      Hello
-    </div>
+    <>
+      <div className="faucet-wrapper">
+        <div className="faucet">
+          <div className="balance-view is-size-2">
+            Current Balance: <strong>10</strong>ETH
+          </div>
+          <button
+            className="btn mr-2"
+            onClick={async () => {
+              const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
+            }}
+          >
+            Enable Ethereum
+          </button>
+          <button className="btn mr-2">Donate</button>
+          <button className="btn mr-2">Withdraw</button>
+        </div>
+      </div>
+    </>
   );
 }
 
