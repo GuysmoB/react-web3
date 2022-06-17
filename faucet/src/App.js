8564442f99f7962e46a1010efd4c63f1e0fa833e
gs337 <remi.brauge@partnre.com>
2022-06-17 14:55:44 +0200

    save

--- faucet/src/App.js
+++ faucet/src/App.js
@@ -1,28 +1,63 @@
 import "./App.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { loadContract } from "./utils/load-contract";
+import detectEthereumProvider from "@metamask/detect-provider";
+import Web3 from "web3";
 
 function App() {
+  const [web3Api, setWeb3Api] = useState({ provider: null, web3: null, contract: null });
+  const [account, setAccount] = useState(null);
+
   useEffect(() => {
-    const loadProvider = async () => {};
+    const loadProvider = async () => {
+      const provider = await detectEthereumProvider();
+      const contract = await loadContract("Faucet", provider);
+
+      if (provider) {
+        setWeb3Api({ web3: new Web3(provider), provider, contract });
+      } else {
+        console.error("Install Metamask noob !");
+      }
+    };
+
+    loadProvider();
   }, []);
+
+  useEffect(() => {
+    const getAccount = async () => {
+      const accounts = await web3Api.web3.eth.getAccounts();
+      setAccount(accounts[0]);
+    };
+
+    web3Api.web3 && getAccount();
+  }, [web3Api.web3]);
 
   return (
     <>
       <div className="faucet-wrapper">
         <div className="faucet">
-          <div className="balance-view is-size-2">
+          <div className="is-flex">
+            <span>
+              <strong>Account :</strong>
+            </span>
+
+            {account ? (
+              <span>{account}</span>
+            ) : (
+              <button
+                className="button is-small"
+                onClick={() => web3Api.provider.request({ method: "eth_requestAccounts" })}
+              >
+                Connect Wallet
+              </button>
+            )}
+          </div>
+          <div className="balance-view is-size-2 mb-4">
             Current Balance: <strong>10</strong>ETH
           </div>
-          <button
-            className="btn mr-2"
-            onClick={async () => {
-              const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
-            }}
-          >
-            Enable Ethereum
-          </button>
-          <button className="btn mr-2">Donate</button>
-          <button className="btn mr-2">Withdraw</button>
+
+          <button className="button is-link mr-2">Donate</button>
+          <button className="button is-primary">Withdraw</button>
         </div>
       </div>
     </>
